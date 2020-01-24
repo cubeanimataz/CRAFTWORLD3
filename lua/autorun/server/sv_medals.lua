7362be1c0450e2b8a0d2b11dbb5a634aed4b309b
cubeanimataz <60117721+cubeanimataz@users.noreply.github.com>
2020-01-24 12:30:40 +0000

    Removed obsolete file "sv_medals.lua"

--- lua/autorun/server/sv_medals.lua
+++
@@ -1,8 +0,0 @@
-util.AddNetworkString( "AddHaloMedal" )
-
-function medal( ply, medal, score )
-	net.Start( "AddHaloMedal" )
-		net.WriteString( medal )
-		net.WriteInt( score or 0, 32 )
-	net.Send( ply )
-end
